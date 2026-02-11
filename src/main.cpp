2169c0486a8e7edb70d1c07ec6a61c97d241faf0
ujjwal srivastava <ujjwal.srivastava_cs.aiml24@gla.ac.in>
2026-02-11 13:53:29 +0530

    ADD screen width and height

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include<Wire.h>
 #include<Adafruit_GFX.h>
 #include<Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 void setup() {
 // write your initialization code here
 }
