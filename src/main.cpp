450e30be5ac085b26536742f87b91b53f9765a92
ujjwal srivastava <ujjwal.srivastava_cs.aiml24@gla.ac.in>
2026-02-11 14:00:26 +0530

    add serial begin

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,12 @@
 #define OLED_ADDRESS 0x3C
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 void setup() {
-// write your initialization code here
+    Serial.begin(9600);
+    if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDRESS)){
+        Serial.println("OLED not found");
+        while (true);
+    }
 }
-
 void loop() {
 // write your code here
 }
