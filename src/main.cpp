a7a7c81858e74306402b7919b8997ca9aab208c0
ujjwal srivastava <ujjwal.srivastava_cs.aiml24@gla.ac.in>
2026-02-11 14:08:00 +0530

    reformat

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,10 @@
 #define SCREEN_HEIGHT 64
 #define OLED_ADDRESS 0x3C
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
 void setup() {
     Serial.begin(9600);
-    if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDRESS)){
+    if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDRESS)) {
         Serial.println("OLED not found");
         while (true);
     }
@@ -18,13 +19,13 @@ void setup() {
     display.setTextSize(1);
     display.setTextColor(SSD1306_WHITE);
 
-    display.setCursor(0,0);
+    display.setCursor(0, 0);
     display.println("Arduino UNO R4");
     display.println("OLED with I2C");
     display.println("Hello Students!");
     display.display();
-
 }
+
 void loop() {
-// write your code here
+    // write your code here
 }
