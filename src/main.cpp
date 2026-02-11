00d028ce80141f66f41d6dcc84510b83fd0c7991
ujjwal srivastava <ujjwal.srivastava_cs.aiml24@gla.ac.in>
2026-02-11 13:56:15 +0530

    create display object

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_ADDRESS 0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 void setup() {
 // write your initialization code here
 }
